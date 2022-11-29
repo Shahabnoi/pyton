096378c59864ec6f34e55f44236ef72403a9af6e
shahab <shahab.noorollahi@gmail.com>
2022-11-29 22:57:15 +0330

    added ExInClass.py

---
+++ ExInClass.py
@@ -0,0 +1,15 @@
+def greet(lang):
+    if lang == 'es':
+        print('Hola')
+    elif lang == 'fr':
+        print('Bonjour')
+    elif lang == 'Fa':
+        print('سلام')
+    else:
+        print('Hello')
+ 
+greet('fr')
+greet('Hola')
+greet('Fa')
+
+
