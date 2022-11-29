280d2e5fc37def569bd708b91d83e6f484308539
shahab <shahab.noorollahi@gmail.com>
2022-11-29 21:56:20 +0330

    added 5slide2.py

---
+++ 5slide2.py
@@ -0,0 +1,48 @@
+
+# slide 2
+def thing():
+    print('Hello')
+    print('Fun')
+ 
+thing()
+print('Zip')
+thing()
+
+# Slide 5
+big = max('Hello world')
+print(big)
+tiny = min('Hello world')
+print(tiny)
+
+# Slide 8
+print(float(99) / 100)
+i = 42
+type(i)
+f = float(i)
+print(f)
+type(f)
+print(1 + 2 * float(3) / 4 - 5)
+
+# Slide 9
+"""
+sval = '123'
+type(sval)
+print(sval + 1)
+"""
+
+ival = int(sval)
+type(ival)
+print(ival + 1)
+nsv = 'hello bob'
+niv = int(nsv)
+
+
+
+
+
+n = 5
+while n > 0 :
+    print(n)
+    n = n - 1
+print('Blastoff!')
+print(n)
