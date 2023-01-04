6c35a61cd8978dc6820baf6387c1871548c514cc
Shahabnoi <117409708+Shahabnoi@users.noreply.github.com>
2023-01-04 23:05:16 +0330

    Create Employee1.py

---
+++ Employee1.py
@@ -0,0 +1,34 @@
+
+
+class Employee:
+      def __init__(self, name, family, WorkingHoursAct):
+          self.name = name
+          self.family = family
+          self.WorkingHoursAct = WorkingHoursAct
+      workHours = 190
+      Salary = 10000
+      Rate = 2
+      ExteraHours = 0                                          
+      ExteraSalary = 0 
+      
+      def pay(self):
+       if self.workHours < self.WorkingHoursAct:                     
+          self.ExteraHours = self.WorkingHoursAct - self.workHours                
+          self.ExteraSalary = (self.ExteraHours*self.Rate)*(self.Salary)             
+          self.Salary=(self.Salary * (self.WorkingHoursAct - self.ExteraHours))+(self.ExteraSalary)         
+          print(int(self.Salary))
+       else:                                                     
+          print(int(self.Salary*self.workHours)) 
+
+      def EmployeePay(self):
+             print( "Mizan daryafti " + self.name,self.family  )
+     
+p1 = Employee("John", "Cage" , 200)
+p2 = Employee("jak", "polman", 190)
+print("hoghogh daryafti " + p1.name,p1.family )    
+p1.pay()  
+print("mibashad")
+print("hoghogh daryafti " + p2.name,p2.family )    
+p2.pay()  
+print("mibashad")
+ 
